f86a847ac486db55abf55b2c19a873e8f31de6ae
Jason_H@TW <80911054+Jason-Hou-TW@users.noreply.github.com>
2021-09-24 17:15:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) {=> {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("KyryuCoco: Good morning MotherFxckers!!");
+    response.end("KyryuCoco Good morning MotherFxckers!!");
 });
 
 const port = process.env.PORT || 1337;
