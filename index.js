a598c5949156498c5dda2d32f73d3a538ac93d3f
Jason_H@TW <80911054+Jason-Hou-TW@users.noreply.github.com>
2021-09-24 15:54:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end(&quot;"Hello World");
-                 });
+    response.end("HelloWorld");
+    response.end("&quot;HelloWorld&quot;");
+});
 
 const port = process.env.PORT || 1337;
 server.listen(port);
