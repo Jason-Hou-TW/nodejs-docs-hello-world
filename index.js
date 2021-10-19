d749ee4af317a07bfbea15d6befad03f28b8c1d7
Jason_H@TW <80911054+Jason-Hou-TW@users.noreply.github.com>
2021-10-19 20:22:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("KSON Fuck this world  , and node.js");
+    response.end("KSON: Fuck this world  , and node.js");
 });
 
 const port = process.env.PORT || 1337;
